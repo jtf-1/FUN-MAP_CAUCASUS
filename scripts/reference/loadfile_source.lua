75bf655cd73a7735d18cdb3d35c347375bc84fa7
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-06-22 14:29:07 +0100

    F-15 waypoints added

--- scripts/reference/loadfile_source.lua
+++ scripts/reference/loadfile_source.lua
@@ -1,3 +1,4 @@
 assert(loadfile("C:\\Users\\rober\\Documents\\GitHub\\FUN-MAP_CAUCASUS\\FunMap_Cauc.lua"))()
 
-assert(loadfile("E:\\GitHub\\FUN-MAP_CAUCASUS\\scripts\\dynamic\\dynamic_loader.lua"))()
+assert(loadfile("D:\\GitHub\\MOOSE_INCLUDE\\Moose_Include_Static\\Moose_.lua"))()
+assert(loadfile("D:\\GitHub\\JTF1-SCRIPTS\\adminmenu.lua"))()
