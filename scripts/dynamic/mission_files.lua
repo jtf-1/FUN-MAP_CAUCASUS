39fff18692aef2bbc17bd4f0e6d15ff3d3a19a66
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-10-02 16:52:44 +0100

    Update mission_files.lua

--- scripts/dynamic/mission_files.lua
+++ scripts/dynamic/mission_files.lua
@@ -3,6 +3,7 @@ __JTFLoader.Include( 'lib/skynet-iads-compiled.lua' )   -- library file
 __JTFLoader.Include( 'core/mission_init.lua' )          -- core file
 __JTFLoader.Include( 'mission_init_data.lua' )
 __JTFLoader.Include( 'core/devcheck.lua' )              -- core file
+--__JTFLoader.Include( 'mission_spawn.lua' )
 __JTFLoader.Include( 'core/missionsrs.lua' )            -- core file
 __JTFLoader.Include( 'missionsrs_data.lua' )
 __JTFLoader.Include( 'core/adminmenu.lua' )             -- core file
@@ -25,6 +26,8 @@ __JTFLoader.Include( 'dynamic_deck_data.lua' )
 __JTFLoader.Include( 'core/missiletrainer.lua' )        -- core file
 __JTFLoader.Include( 'missiletrainer_data.lua' )
 __JTFLoader.Include( 'markspawn.lua' )
+__JTFLoader.Include( 'markspawn_templates.lua' )
+--__JTFLoader.Include( 'markspawn_data.lua' )
 __JTFLoader.Include( 'core/Hercules_Cargo.lua' )        -- core file
 __JTFLoader.Include( 'ablesentry.lua' )
 __JTFLoader.Include( 'core/mission_end.lua' )           -- core file    
